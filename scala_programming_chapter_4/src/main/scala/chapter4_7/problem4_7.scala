27820e8cfe0871354dc12ea07ce91bd99fd3dffe
강성혁 <secondray1234@gmail.com>
2018-01-19 18:45:41 +0900

    chapter5

--- scala_programming_chapter_4/src/main/scala/chapter4_7/problem4_7.scala
+++ scala_programming_chapter_4/src/main/scala/chapter4_7/problem4_7.scala
@@ -15,8 +15,6 @@ object problem4_7 {
     for ((key, value) <- props) {
       printf("%-" + (longestLength + 1) +"s | %s%n", key, value)
     }
-    println("hi")
-    print(("hello yo"))
   }
 
 }
---
+++ scala_programming_chapter_5/src/main/scala/chapter5_7/problem5_7.scala
@@ -0,0 +1,7 @@
+package chapter5_7
+
+object problem5_7 {
+  def main(args: Array[String]): Unit = {
+    println("fff")
+  }
+}
