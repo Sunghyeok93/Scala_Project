9bc2765c51f62fe340e8b1a3a21804a4bcc7d4be
강성혁 <secondray1234@gmail.com>
2018-02-05 13:46:28 +0900

    scala_programming_class

---
+++ scala_programming_chapter_8/build.sbt
@@ -0,0 +1,5 @@
+name := "scala_programming_chapter_8"
+
+version := "0.1"
+
+scalaVersion := "2.12.4"
---
+++ scala_programming_chapter_8/src/main/scala/chapter8_1/problem8_1.scala
@@ -0,0 +1,7 @@
+package chapter8_1
+
+object problem8_1 {
+  def main(args: Array[String]): Unit = {
+    
+  }
+}
