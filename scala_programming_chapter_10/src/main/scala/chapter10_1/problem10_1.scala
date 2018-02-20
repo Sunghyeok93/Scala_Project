ee9e5fa2886c4fbf776cd19cd7969f3579153ba2
강성혁 <secondray1234@gmail.com>
2018-02-20 15:00:11 +0900

    scala_programming

--- scala_programming_chapter_10/src/main/scala/chapter10_1/problem10_1.scala
+++ scala_programming_chapter_10/src/main/scala/chapter10_1/problem10_1.scala
@@ -1,9 +1,12 @@
 package chapter10_1
 
+import java.util.logging.Logger
+
 
 object problem10_1 {
   def main(args: Array[String]): Unit = {
     println("hellop")
+    new Logger("hiiiii")
   }
 
 
